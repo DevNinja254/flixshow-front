6fa9835d2c0581ec62c78f11d4734ec1a2300619
Augustine Imbali <aga@devninja254.devninja254.io>
2025-04-06 14:50:27 +0300

    add betting, like and comment models and api/s

--- src/js/api.js
+++ src/js/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://admin.flixshow.xyz/api/v1/"
+    baseURL: "https://admin.kingstonemovies.xyz/api/v1/"
     // baseURL: "http://localhost:8000/api/v1/"
 })
 
